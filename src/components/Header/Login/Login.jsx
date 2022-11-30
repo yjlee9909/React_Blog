73d486176828cbbbbe9b3b4d4679dd88f023374f
yjlee9909 <yjlee9909@naver.com>
2022-11-30 17:50:39 +0900

    add: 프로필 사진 변경

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return (
-    <>
-      {/* 로그인 */}
-      <li className="profile-img">
-              <Link to="/">
-                <img src="https://github.com/weniv/react-blog/blob/react/public/assets/profile.jpg?raw=true" alt="My Page" />
-              </Link>
+    return (
+        <>
+            {/* 로그인 */}
+            <li className="profile-img">
+                <Link to="/">
+                    <img src="https://cdn.pixabay.com/photo/2022/11/17/14/00/astrology-7598174_960_720.png" alt="My Page" />
+                </Link>
             </li>
             <li>
-              <Link to="/" className="button">
-                <img src="./assets/icon-modify-white.svg" alt="" />
-                <span>Write</span>
-              </Link>
+                <Link to="/" className="button">
+                    <img src="./assets/icon-modify-white.svg" alt="" />
+                    <span>Write</span>
+                </Link>
             </li>
             <li>
-              <button className="button white">
-                <img src="./assets/icon-logout.svg" alt="" />
-                <span>Logout</span>
-              </button>
+                <button className="button white">
+                    <img src="./assets/icon-logout.svg" alt="" />
+                    <span>Logout</span>
+                </button>
             </li>
-        {/* //로그인 */}
-    </>
-  )
+            {/* //로그인 */}
+        </>
+    );
 }
--- src/components/Main/author/Author.jsx
+++ src/components/Main/author/Author.jsx
@@ -1,12 +1,14 @@
-import "./author.css"
+import "./author.css";
 
-export default function Author({userName, profileImg, created}) {
-  return (
-    <dl class="author-wrap">
-      <dt class="a11y-hidden">Author</dt>
-      <dd class="author"><img src="https://github.com/weniv/react-blog/blob/react/public/assets/profile.jpg?raw=true" alt="" /> {userName}</dd>
-      <dt class="a11y-hidden">Created</dt>
-      <dd class="created">{created}</dd>
-    </dl>
-  )
+export default function Author({ userName, profileImg, created }) {
+    return (
+        <dl class="author-wrap">
+            <dt class="a11y-hidden">Author</dt>
+            <dd class="author">
+                <img src="https://cdn.pixabay.com/photo/2022/11/17/14/00/astrology-7598174_960_720.png?raw=true" alt="" /> {userName}
+            </dd>
+            <dt class="a11y-hidden">Created</dt>
+            <dd class="created">{created}</dd>
+        </dl>
+    );
 }
